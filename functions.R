36ec31d1addee31a0dfba619b3091f6a9fc4d9eb
Alberto Maria Chierici <amc@mbpdialchierici.home>
2015-11-29 22:03:19 +0000

    added function to calculate stats for hexagonal area

--- functions.R
+++ functions.R
@@ -25,3 +25,27 @@ calcLatLong <- function(lat, longitude, distance, bearing=0){
 }
 #test
 # calcLatLong(-0.0430042, 51.5195786, 250) #check on gmap
+
+
+
+# Calc crime stats for hexagon --------------------------------------------
+
+hexCrimeStats <- function(Ox, Oy, d, categories=c("anti-social-behaviour", "bicycle-theft", "burglary", "criminal-damage-arson", "drugs", "other-theft", "possession-of-weapons", "public-order", "robbery", "shoplifting", "theft-from-the-person", "vehicle-crime", "violent-crime", "other-crime")){
+  # d <- 250 #in metres
+  p1 <- calcLatLong(Ox, Oy, d, 0)
+  p2 <- calcLatLong(Ox, Oy, d, 60)
+  p3 <- calcLatLong(Ox, Oy, d, 120)
+  p4 <- calcLatLong(Ox, Oy, d, 180)
+  p5 <- calcLatLong(Ox, Oy, d, 240)
+  p6 <- calcLatLong(Ox, Oy, d, 300)
+  
+  # Custom area example: https://data.police.uk/api/crimes-street/all-crime?poly=52.268,0.543:52.794,0.238:52.130,0.478&date=2013-01
+  CrimeStats <- fromJSON(paste0(
+    "https://data.police.uk/api/crimes-street/all-crime?poly=",
+    paste0(p1[1], ",", p1[2], ":", p2[1], ",", p2[2], ":", p3[1], ",", p3[2], ":", p4[1], ",", p4[2], ":", p5[1], ",", p5[2], ":", p6[1], ",", p6[2])#,
+    #   "&date=", #default to last month, use this to specify date
+    #   substr(Sys.Date()-61, 1, 7) # 2 months ago is the most recent update?
+  ))
+  
+  melt(table(CrimeStats$category))
+}
